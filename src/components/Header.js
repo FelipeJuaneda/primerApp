bfd833b419badc365b9e3b7ef6e2add7b13bdd64
felipe.juaneda <felipejua9@gmail.com>
2022-04-07 19:23:21 -0300

    primeros pasos en la app, creado header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './Header.css';
 
+
 function HeaderComp(){
     return <header className="header">
         <span className="logo">LOGO</span>
         <ul className="listaDesordenada">
             <li> <a href="https://github.com/FelipeJuaneda">Inicio</a> </li>
-            <li><a href="https://github.com/FelipeJuaneda">Peliculas</a></li>
-            <li><a href="https://github.com/FelipeJuaneda">Series</a></li>
+            <li><a href="https://github.com/FelipeJuaneda">Productos</a></li>
+            <li><a href="https://github.com/FelipeJuaneda">Contacto</a></li>
         </ul>
     </header>
 };
