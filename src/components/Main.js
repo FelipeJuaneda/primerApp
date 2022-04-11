09d50d15ac19bdf94bb244c09ca81ffe9735e77d
felipe.juaneda <felipejua9@gmail.com>
2022-04-11 04:20:14 -0300

    cambios en main y medias queries

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,19 @@ import videoFondo from './videos/heladoDerretido.mp4';
 
 function MainComp() {
     return <div className="mainVideoCont">
-            <video autoplay muted loop className="videoCont">
+            <video  autoPlay muted loop className="videoCont">
             <source src={videoFondo} type="video/mp4"/>
         </video>
+        <div className="mainTituloCont">
+            <h1 className="titulo">Disfruta del mejor Helado</h1>
+            <p className="parrafoTitulo">Artesanal por excelencia, con sabor, textura y aroma <span>único</span></p>
+            <button className="cta">
+                <span className="hover-underline-animation"> Descubrir Sabores </span>
+                <svg id="arrow-horizontal" xmlns="http://www.w3.org/2000/svg" width="30" height="10" viewBox="0 0 46 16">
+                    <path id="Path_10" data-name="Path 10" d="M8,0,6.545,1.455l5.506,5.506H-30V9.039H12.052L6.545,14.545,8,16l8-8Z" transform="translate(30)"></path>
+                </svg>
+            </button>
+        </div>
     </div>
 };
 
