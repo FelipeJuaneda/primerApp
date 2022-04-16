241e9492e6fc828f62fc9414d4198a9cd63c52e4
felipe.juaneda <felipejua9@gmail.com>
2022-04-16 19:20:54 -0300

    menu burger creado

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     titulo.style.color = "green";
   };
 
+  /* -------------------------------------------------- */
   return (
     <div className="App">
       <NavBar menuBurger={menuBurger} />
