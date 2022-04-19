c0c9dbc4335fb9f3061526721235aa4d903b1e56
felipe.juaneda <felipejua9@gmail.com>
2022-04-18 23:01:07 -0300

    contador creado - todavia falta condicional

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,23 +1,25 @@
-import {React, useState} from 'react';
+import { React, useState } from "react";
 
+const ItemCount = () => {
+  const [valor, setValor] = useState(0);
 
-const Contador = () => {
-    const [valor, setValor] = useState(0);
-    const aumentar = ()=>{
-        setValor(valor + 1);
+  const aumentar = () => {
+    setValor(valor + 1);
+  };
+  const disminuir = () => {
+    setValor(valor - 1);
+    if (valor <= 0) {
+      console.log("funca");
     }
-    const disminuir = ()=>{
-        setValor(valor - 1);
-    }
-
+  };
 
   return (
     <div>
-        <button onClick={disminuir}>-</button>
-        <b>{valor}</b>
-        <button onClick={aumentar}>+</button>
+      <button onClick={disminuir}>-</button>
+      <b>{valor}</b>
+      <button onClick={aumentar}>+</button>
     </div>
-  )
-}
+  );
+};
 
-export default Contador
+export default ItemCount;
